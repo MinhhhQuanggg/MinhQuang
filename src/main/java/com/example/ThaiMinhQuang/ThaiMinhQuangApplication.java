5a1e1f8f4c990fcfa5bea3174bfd6587eb527f94
Wuang-ITCode <qminh0733@gmail.com>
2026-03-26 10:48:39 +0700

    Complete CI/CD setup

---
+++ src/main/java/com/example/ThaiMinhQuang/ThaiMinhQuangApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThaiMinhQuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiMinhQuangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiMinhQuangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThaiMinhQuang/ThaiMinhQuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThaiMinhQuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaiMinhQuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
